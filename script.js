1676963a1538b6de7615d21f49c0e49da9305d83
prashantdubeypng <vinoddubey6059@gmail.com>
2025-02-02 12:40:46 +0530

    initall

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const submitButton = document.querySelector('.submit-btn');
+
+    submitButton.addEventListener('mouseover', function() {
+        this.style.transform = 'scale(1.05)';
+    });
+
+    submitButton.addEventListener('mouseleave', function() {
+        this.style.transform = 'scale(1)';
+    });
+});
